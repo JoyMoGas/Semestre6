90abdde0f302539eda306b16a7a0fb69c3bbe1c2
Alumnos <Alumnos@05G203-PC25.unison.mx>
2025-01-15 12:56:30 -0700

    Class 3

---
+++ Ocho_Reinas/js/scripts.js
@@ -0,0 +1,29 @@
+// Declaracion de variables globales
+var contador = 0;
+var queen = "./img/queen.png"
+
+function showQueen(celda) {
+  if (window.getComputedStyle(celda).backgroundImage=='none') {
+    if (contador < 8) {
+      /*INTERPOLACION*/
+      celda.style=`
+              background-image: url(${queen});
+              background-repeat: no-repeat;
+              background-position: center;`;
+              contador++;
+    }
+  } else {
+    celda.style=`
+              background-image: none;`;
+              contador--;
+  }
+}
+
+function changeColor(r, c) { /* Renglones Columnas*/
+  var celda = document.getElementById("tablero");
+  var r1 = r, c1 = c, r2 = r, c2 = c;
+  var r3 = r, c3 = c, r4 = r, c4 = c;
+  for (let i = 0; i < array.length; i++) { /* Let para variables locales (dentro de una funcion) */
+    
+  }
+}
