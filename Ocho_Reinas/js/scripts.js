56e0bfba65100db91f62fc3408f47613343aa50d
Alumnos <Alumnos@05G203-PC25.unison.mx>
2025-01-20 12:39:07 -0700

    Finished?

--- Ocho_Reinas/js/scripts.js
+++ Ocho_Reinas/js/scripts.js
@@ -27,9 +27,17 @@ function changeColor(r, c) { /* Renglones Columnas*/
     celda.rows[r].cells[i].style.backgroundColor="red";
     celda.rows[i].cells[c].style.backgroundColor="red";
 
-    if (r1 < 8 && c1 < 8) {
+    if (r1 < 8 && c1 < 8) 
       celda.rows[r1++].cells[c1++].style.backgroundColor="red";
-    }
+  
+    if (r2 > -1 && c2 < 8) 
+      celda.rows[r2--].cells[c2++].style.backgroundColor="red";
+    
+    if (r3 > -1 && c3 > -1) 
+      celda.rows[r3--].cells[c3--].style.backgroundColor="red";
+  
+    if (r4 < 8 && c4 > -1) 
+      celda.rows[r4++].cells[c4--].style.backgroundColor="red";
   }
 }
 
